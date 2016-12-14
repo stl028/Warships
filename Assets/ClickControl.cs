34a4bd6a9bb86e92e3610614c11159b47713f43f
stl028 <stevenlee1996@gmail.com>
2016-12-14 15:51:12 -0800

    Added clickControl script

---
+++ Assets/ClickControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickControl : MonoBehaviour {
+    public GameObject clickedGameObj;
+
+	public void SelectedShip(GameObject clickedOn)
+    {
+        clickedGameObj = clickedOn;
+        print(clickedGameObj.name);
+        Update();
+
+    }
+
+    void Update()
+    {
+        Vector3 mouseScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z);
+
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+
+        transform.LookAt(mouseWorldPosition, Vector3.right);
+    }
+}
