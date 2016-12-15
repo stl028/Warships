7584c9fbaa0933a395ace8378ab21b8c87d232bf
Raigecki <edwin.ejong@gmail.com>
2016-12-14 22:03:50 -0800

    trial

--- Assets/ShipControls.cs
+++ Assets/ShipControls.cs
@@ -6,19 +6,24 @@ public class ShipControls : MonoBehaviour {
 
     bool directionOn = false;
 
+	public GameObject clickedObj;
     public ClickControl control0;
     public ClickControl control1;
 
 	// Use this for initialization
 	void Start () {
+
         control0 = GameObject.Find("ship0").GetComponent<ClickControl>();
         control1 = GameObject.Find("ship1").GetComponent<ClickControl>(); 
 	}
 
     void Update()
-    {   if (Input.GetMouseButtonDown(0))
+    {   
+		/*
+		if (Input.GetMouseButtonDown(0))
         {
             directionOn = true;
+			clickedObj = GetClickedGameObject ();
         }
         if (Input.GetMouseButtonDown(1))
         {
@@ -30,6 +35,30 @@ public class ShipControls : MonoBehaviour {
             control0.SelectedShip(this.transform.gameObject);
             control1.SelectedShip(this.transform.gameObject);
         }
+		if (ship0.
+
+*/
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hitInfo = new RaycastHit();
+			bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+			if (hit) 
+			{
+				Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+				var name = hitInfo.gameObject.name;
+
+				if (hitInfo.transform.gameObject.tag == "ship0")
+				{
+					control0.SelectedShip(this.transform.gameObject);
+				} else {
+					control1.SelectedShip(this.transform.gameObject);
+				}
+			} else {
+				Debug.Log("No hit");
+			}
+			Debug.Log("Mouse is down");
+		} 
     }
 	
 	
