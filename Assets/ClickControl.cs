ca31280f1d2c0a7d090894ab9e13af1d598e1593
Jimmy Nguyen <holycrapanalt@gmail.com>
2016-12-20 16:51:02 -0800

    Completed rotating arrow

--- Assets/ClickControl.cs
+++ Assets/ClickControl.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class ClickControl : MonoBehaviour {
     
 	ShipControls control;
 	public Camera cam;
+    Thread dirThread;
 
+    //For now the cone width of all ships is 30
+    float coneWidth = Mathf.PI / 6;
 
-	//For figuring out which state we're in
-	int leftClick = 0;
+    float tempDirection = 0;
+
+    //For figuring out which state we're in
+    int leftClick = 0;
 	void Start() {
 
 	}
 
     void Update()
     {
-
         //turning the ship
         if (leftClick == 1)
         {
@@ -54,16 +59,33 @@ public class ClickControl : MonoBehaviour {
 
         ///////////////////////////////////For Selecting the direction/////////////////////////////////////
 
-        if (Input.GetMouseButtonDown(0) && leftClick == 1)
+        else if (Input.GetMouseButtonDown(0) && leftClick == 1)
         {
+            Debug.Log("LeftClick1");
+            leftClick++;
+
             Vector3 shipPos = control.GetShipPosition();
             Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
             float theta = ConeDirection(shipPos, mousePos);
+            float coneStart = theta - coneWidth / 2;
+            float coneEnd = theta + coneWidth / 2;
 
+            tempDirection = theta;
+            float spacing = Mathf.PI / 180;
+
+            dirThread = new Thread(() => startDirectionCounter(spacing, coneStart, coneEnd));
+            dirThread.Start();
 
         }
 
-        if (Input.GetMouseButtonDown(1))
+        else if (Input.GetMouseButtonDown(0) && leftClick == 2)
+        {
+            ++leftClick;
+            Debug.Log("Final Float:" + tempDirection.ToString());
+            dirThread.Abort();
+        }
+
+        if (Input.GetMouseButtonDown(1) && leftClick != 0)
         {
 
             leftClick--;
@@ -77,5 +99,51 @@ public class ClickControl : MonoBehaviour {
         float slope = (mousePos.y - shipPos.y) / (mousePos.x - shipPos.x);
 
         return Mathf.Atan(slope);
+    }
+
+    public void startDirectionCounter(float spacing, float coneStart, float coneEnd)
+    {
+
+        bool direction = false;
+
+        while (leftClick == 2)
+        {
+            Debug.Log("in inf loop");
+            Debug.Log(leftClick.ToString());
+
+
+            if (tempDirection + spacing < coneEnd && !direction)
+            {
+                tempDirection += spacing;
+            }
+            else if (tempDirection + spacing > coneEnd && !direction)
+            {
+                tempDirection -= spacing;
+                direction = true;
+            }
+            else if (tempDirection - spacing > coneStart && direction)
+            {
+                tempDirection -= spacing;
+            }
+            else if (tempDirection - spacing < coneStart && direction)
+            {
+                tempDirection += spacing;
+                direction = false;
+            }
+            /*
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("breakLeftClick");
+                leftClick++;
+                //break;
+            }
+            else if (Input.GetMouseButtonDown(1) && leftClick == 2)
+            {
+                Debug.Log("breakRightClick");
+                break;
+            }
+            */
+            Debug.Log("bot of inf loop");
+        }
     }
 }
