5574483835c138fd35a35fa7ecf3ec672a92ff57
stl028 <stevenlee1996@gmail.com>
2016-12-29 18:31:53 -0800

    Working on collisions

--- Assets/Scripts/ShipPhysics.cs
+++ Assets/Scripts/ShipPhysics.cs
@@ -7,7 +7,42 @@ public class ShipPhysics : MonoBehaviour
 {
  
 
-	void OnCollisionEnter(Collision col) {
-        Destroy(col.gameObject);
+	void OnTriggerEnter2D(Collider2D col) {
+        Debug.Log("Collided!");
+        if (col.gameObject.tag == "Ship")
+        {
+            Debug.Log("Name of ship: " + col.gameObject.name);
+            GameObject hitShip = GameObject.Find(col.gameObject.name);
+            Destroy(hitShip);
+        }
 	}
+
+    /*void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Collided!");
+        if (col.gameObject.tag == "Ship")
+        {
+            Destroy(col.gameObject);
+        }
+    }*/
+
+    /*void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Collided!");
+        if (col.gameObject.tag == "Ship")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collided!");
+        if (col.gameObject.tag == "Ship")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+    */
+
 }
