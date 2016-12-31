b767ac6c234cded04ec3edea2329c75dd2199bfe
Raigecki <edwin.ejong@gmail.com>
2016-12-31 15:43:10 -0800

    LaunchArc

---
+++ Assets/Scripts/LaunchArc.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaunchArc : MonoBehaviour {
+
+	public Image launchArc;
+	public Image launchArrow;
+
+	public void ChangeArcPos(Vector3 pos) {
+
+		launchArc.transform.position = pos;
+	}
+
+	public void ChangeArrPos(Vector3 pos) {
+
+	}
+
+	public LaunchArc( ref Image arc) {
+		launchArc = arc;
+	}
+
+	public void LaunchArrow( ref Image arrow) {
+		launchArrow = arrow;
+	}
+
+}
